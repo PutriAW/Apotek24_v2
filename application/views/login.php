c44d074c85637a502e84b2510411f4585f004437
Faisal Ridwan <faisalridwansiregar@gmail.com>
2020-04-17 22:43:47 +0700

    perbaiki tampilan login

--- application/views/login.php
+++ application/views/login.php
@@ -27,18 +27,18 @@
     <!-- Outer Row -->
     <div class="row justify-content-center">
 
-      <div class="col-xl-10 col-lg-12 col-md-9">
+      <div class="col-xl-5 ">
 
         <div class="card o-hidden border-0 shadow-lg my-5">
           <div class="card-body p-0">
             <!-- Nested Row within Card Body -->
             <div class="row">
-              <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
-              <div class="col-lg-6">
+              <div class="col-lg-12">
                 <div class="p-5">
                   <div class="text-center">
                     <h1 class="h4 text-gray-900 mb-4">Welcome to Apotek24</h1>
                   </div>
+                  
                   <form class="user" action="<?= base_url(); ?>login/aksi_login" method="post">
                     <div class="form-group">
                       <input type="text" name="username" class="form-control form-control-user" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Enter Username...">
